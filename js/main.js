4ef5f2b3387599ad24846d28c191698a8097426a
SectorMind <66206867+SectorMind@users.noreply.github.com>
2024-08-14 15:52:45 +0200

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -22,10 +22,8 @@
     }
 
     /* Modal window */
-    // console.log("Modal window")
     const modal = document.querySelector('.modal');
     const modalButton = document.querySelector('.sing-in');
-    // console.log(modal, modalButton)
 
     modalButton.addEventListener('click', openModal);
     modal.addEventListener('click', closeModal);
@@ -51,84 +49,16 @@
         }
     });
 
-    /* tabs */
-
-    // const tabControls = document.querySelector('.tab-controls');
-
-    // tabControls.addEventListener('click', toggleTab);
-
-    // function toggleTab(e) {
-    //     const tabControl = e.target.closest('.tab-controls__link');
-
-    //     if (!tabControl) return;
-    //     e.preventDefault();
-    //     if (tabControl.classList.contains('tab-controls__link--active')) return;
-
-    //     const tabContentID = tabControl.getAttribute('href');
-    //     const tabContent = document.querySelector(tabContentID);
-    //     const activeControl = document.querySelector('.tab-controls__link--active');
-    //     const activeContent = document.querySelector('.tab-content--show');
-
-    //     if (activeControl) {
-    //         activeControl.classList.remove('tab-controls__link--active');
-    //     }
-    //     if (activeContent) {
-    //         activeContent.classList.remove('tab-content--show');
-    //     }
-
-    //     tabControl.classList.add('tab-controls__link--active');
-    //     tabContent.classList.add('tab-content--show');
-    // }
-
-    /* accordion */
-
-    // const accordionLists = document.querySelectorAll('.accordion-list');
-
-    // accordionLists.forEach(el => {
-
-    //     // document.querySelector('.accordion-list__item--opened .accordion-list__content').style.maxHeight = document.querySelector('.accordion-list__item--opened .accordion-list__content').scrollHeight + 'px';
-
-    //     el.addEventListener('click', (e) => {
-
-    //         const accordionList = e.currentTarget;
-    //         const accordionOpenedItem = accordionList.querySelector('.accordion-list__item--opened');
-    //         const accordionOpenedContent = accordionList.querySelector('.accordion-list__item--opened .accordion-list__content');
-
-    //         const accordionControl = e.target.closest('.accordion-list__control');
-    //         if (!accordionControl) return;
-    //         e.preventDefault();
-    //         const accordionItem = accordionControl.parentElement;
-    //         const accordionContent = accordionControl.nextElementSibling;
-
-    //         if (accordionOpenedItem && accordionItem != accordionOpenedItem) {
-    //             accordionOpenedItem.classList.remove('accordion-list__item--opened');
-    //             accordionOpenedContent.style.maxHeight = null;
-    //         }
-    //         accordionItem.classList.toggle('accordion-list__item--opened');
-
-    //         if (accordionItem.classList.contains('accordion-list__item--opened')) {
-    //             accordionContent.style.maxHeight = accordionContent.scrollHeight + 'px';
-    //         } else {
-    //             accordionContent.style.maxHeight = null;
-    //         }
-    //     });
-    // })
-
     /* slider-rewues */
 
     const swiper = new Swiper('.testimonials__slider', {
-
-        spaceBetween: 15,
-        slidesPerView: 2,
-
         // Optional parameters
         // direction: 'vertical',
         // loop: true,
 
         // If we need pagination
         pagination: {
-            el: '.testimonials__pagination',
-            type: 'fraction'
+            el: '.swiper-pagination',
         },
 
         // Navigation arrows
@@ -138,27 +68,9 @@
         },
 
         // And if we need scrollbar
-        // scrollbar: {
-        //     el: '.swiper-scrollbar',
-        // },
-
-        // breakpoints: {
-        //     // when window width is >= 320px
-        //     // 320: {
-        //     //     slidesPerView: 2,
-        //     //     spaceBetween: 20
-        //     // },
-        //     // when window width is >= 640px
-        //     601: {
-        //         slidesPerView: 3,
-        //     },
-        //     801: {
-        //         spaceBetween: 32,
-        //     },
-        //     1101: {
-        //         slidesPerView: 4,
-        //     },
-        // }
+        scrollbar: {
+            el: '.swiper-scrollbar',
+        },
     });
 
     
